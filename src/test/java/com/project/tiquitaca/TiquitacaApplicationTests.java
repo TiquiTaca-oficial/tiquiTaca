e478aaf16945b1a857ba7ff5fa38afda6aaa9b0a
hyerim108 <batlove108@naver.com>
2024-04-14 19:22:42 +0900

    feat : tiquitaca 초기 설정

---
+++ src/test/java/com/project/tiquitaca/TiquitacaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.tiquitaca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiquitacaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
